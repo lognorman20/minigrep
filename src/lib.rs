2caf988e9a70b876bb23815e6385133946be50ed
Logan Norman <logannormanss@gmail.com>
2022-07-03 01:33:50 -0700

    passed first test

--- src/lib.rs
+++ src/lib.rs
@@ -9,19 +9,28 @@ mod tests {
     fn one_result() { 
         let query = "duct";
         let contents = "\
-            Rust:
-            safe, fast, productive.
-            Pick three.";
-        assert_eq!(vec!["safe, fast proudctive."], search(query, contents));
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut output = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            output.push(line);
+        }
+    }
+    output
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
     Ok(())
 }
 
